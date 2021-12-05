54b84e3079eafb9a4cd9ba9cbe51e4c894188289
jrperez8 <betelgsoft@gmail.com>
2021-12-04 19:31:08 -0500

    Version 2

--- App.py
+++ App.py
@@ -17,6 +17,14 @@ def Index():
     data = cur.fetchall()
     return render_template('index.html', customers = data)
 
+@app.route('/facturar/<id>')
+def facturar(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM facturas WHERE id_cli = {0}'.format(id))
+    data = cur.fetchall()
+    return render_template('facturar.html', invoices = data)
+
+
 @app.route('/add', methods=['POST'])
 def add_contact():
     if request.method == 'POST':
@@ -29,14 +37,36 @@ def add_contact():
         mysql.connection.commit()
         flash('Contacto Agregado de Forma Correcta')
         return redirect(url_for('Index'))
-    
 
+
+@app.route('/addfact', methods=['POST'])
+def add_factura():
+    if request.method == 'POST':
+      date = request.form['date']
+      id = request.form['idcli']
+      price = request.form['price']
+      balance = request.form['balance']
+      cur = mysql.connection.cursor()
+      cur.execute('INSERT INTO facturas (fecha, id_cli, price, balance) VALUES (%s, %s, %s, %s)', (date, id, price, balance))
+      mysql.connection.commit()
+      flash('Factura Agregada de Forma Correcta')
+      return redirect(url_for('Index'))
+       
+    
 @app.route('/edit/<id>')
 def editContact(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM customer WHERE id = {0}'.format(id))
     data = cur.fetchall()
     return render_template('edit.html', customers = data[0])
+
+
+@app.route('/editfact/<number>')
+def editFactura(number):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM facturas WHERE number = {0}'.format(number))
+    data = cur.fetchall()
+    return render_template('editfact.html', invoices = data)
 
 @app.route('/update/<id>', methods = ['POST'])
 def update(id):
@@ -62,6 +92,14 @@ def delete_contact(id):
     cur.execute('DELETE FROM customer WHERE id = {0}'.format(id))
     mysql.connection.commit()
     flash('Contacto Removido Satisfactoriamente')
+    return redirect(url_for('Index'))
+
+@app.route('/deletefact/<string:number>')
+def delete_factura(number):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM facturas WHERE number = {0}'.format(number))
+    mysql.connection.commit()
+    flash('Factura Removida Satisfactoriamente')
     return redirect(url_for('Index'))
 
 if __name__ == '__main__':
